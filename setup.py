829e6d1866f14e49e852260f914411cc445c6991
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2024-11-12 12:43:09 +0100

    Minor updates

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setuptools.setup(
         'Intended Audience :: Developers',
         'Intended Audience :: Other Audience',
 
-        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.12',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Typing :: Typed',
